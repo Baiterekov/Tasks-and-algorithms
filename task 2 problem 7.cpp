a9c9d490a54bf8f1e3c057782adf53868fbe26c7
Bekali <66372333+Baiterekov@users.noreply.github.com>
2022-03-12 00:48:08 +0600

    Add files via upload

---
+++ task 2 problem 7.cpp
@@ -0,0 +1,56 @@
+﻿// задание 1  задача 7 Найти минимальный элемент исходного стека и создать новый стек, 
+// путем умножения элементов исходного стека на найденный 
+// минимальный элемен
+
+#include <iostream>
+#include <stack>								// добавляем библиотеку для стека
+
+using namespace std;
+
+int main() {
+	stack <int> first_stack;						// объявляем стек
+	int enter_number;
+
+	while (true)									// пока цикл while true заполняем стек
+	{
+		cout << "enter -99 to exit or enter number to add for stack: ";   // тут наверное все понятно
+		cin >> enter_number;						// добавляем новые элементы для стека, добавляет пользователь
+		if (enter_number == -99) break;				// если enter_number ровно -99 выходим из цикла
+		cout << endl;
+		first_stack.push(enter_number);				// заполняем стек новыми элементами
+
+	}
+
+	cout << "First stack size: " << first_stack.size();		// выводим размер стека
+	enter_number = size(first_stack);
+	int min_num = first_stack.top();						// минимальная число пока равен самому первому элементу стека
+	cout << min_num << endl;
+	for (int i = 0; i < enter_number; i++) {			// цикл (ну я не знаю что писать просто объявил цикл)
+		if (min_num > first_stack.top()) {					// есди минимальная число больше элемента стека тут написано
+			min_num = first_stack.top();					// минимальный элемент равно на элемент стека
+		}
+		cout << min_num<<endl;
+		first_stack.pop();									// удаляем элемент стека что бы получить другие элементы стека по очереди
+	}
+	cout << endl;
+	cout << "Minimum number in first stack: " << min_num << endl;	// тут типа показываем минимальную элемент
+
+	cout << "ente size stack: " << endl;
+	stack <int> new_stack;									// создаем новый стек
+	int size_stack, rand_num;
+	cin >> size_stack;										// даем размер новому стеку
+
+
+	for (int i = 0; i < size_stack; i++) {				// в этом цикле заполняем новый стек рандомными элементами
+		rand_num = 1 + rand() % 100;
+		cout << "random numbers to fit new stack: " << rand_num << endl;	// выводим рандомные числа
+		new_stack.push(rand_num * min_num);								// заполняем стек фомулой рандомные числа * минимальная число предедущего стека
+	}
+	cout << "New stack size: " << new_stack.size() << endl;
+
+	cout << "New stack elements: " << endl;
+	for (int i = 0; i < size_stack; i++) {			// выводим и удаляем элементы стека
+		cout << new_stack.top() << endl;
+		new_stack.pop();
+	}
+}
---
+++ task 3 problem 7.cpp
@@ -0,0 +1,61 @@
+﻿/* задание 3  задача 7 Название фирмы, название услуги, 
+продолжительность выполнения, цена
+Определить среднее цену по всем видам 
+услуг, затем выведите все фирмы, цена 
+услуги которых ниже среднего значения.*/
+
+
+#include <iostream>
+
+using namespace std;
+
+struct business {
+	string company_name;
+	string service_name;
+	string duration_of_execution;
+	float cost;
+};
+
+int main() {
+	business company1 = { "first company", "service 1", "5 sec", 5.00 };
+	business company2 = { "second company", "service 1", "10 sec", 10.00 };
+	business company3 = { "third company", "service 1", "6 sec", 6.00 };
+	business company4 = { "forth company", "service 1", "8 sec", 8.00 };
+	business company5 = { "fifth company", "service 1", "12 sec", 12.00 };
+	business company6 = { "sixth company", "service 1", "9 sec", 9.00 };
+	business company7 = { "seventh company", "service 1", "15 sec", 15.00 };
+	business company8 = { "eighth company", "service 1", "4 sec", 4.00 };
+
+	float average_cost = (company1.cost + company2.cost + company3.cost + company4.cost + company5.cost + company6.cost + company7.cost + company8.cost) / 8;
+
+	cout << "Average cost: " << average_cost << endl;
+	cout << endl;
+
+	if (average_cost > company1.cost) {
+		cout << company1.company_name << " " << company1.cost << "$ " << company1.service_name << " " << company1.duration_of_execution << endl;
+	}
+	if (average_cost > company2.cost) {
+		cout << company2.company_name << " " << company2.cost << "$ " << company2.service_name << " " << company2.duration_of_execution << endl;
+	}
+	if (average_cost > company3.cost) {
+		cout << company3.company_name << " " << company3.cost << "$ " << company3.service_name << " " << company3.duration_of_execution << endl;
+	}
+	if (average_cost > company4.cost) {
+		cout << company4.company_name << " " << company4.cost << "$ " << company4.service_name << " " << company4.duration_of_execution << endl;
+	}
+	if (average_cost > company5.cost) {
+		cout << company5.company_name << " " << company5.cost << "$ " << company5.service_name << " " << company5.duration_of_execution << endl;
+	}
+	if (average_cost > company6.cost) {
+		cout << company6.company_name << " " << company6.cost << "$ " << company6.service_name << " " << company6.duration_of_execution << endl;
+	}
+	if (average_cost > company7.cost) {
+		cout << company7.company_name << " " << company7.cost << "$ " << company7.service_name << " " << company7.duration_of_execution << endl;
+	}
+	if (average_cost > company8.cost) {
+		cout << company8.company_name << " " << company8.cost << "$ " << company8.service_name << " " << company8.duration_of_execution << endl;
+	}
+
+	system("pause");
+	return 0;
+}
