2b6eeb8feb53fe40ea81cdbdcb50b1b10228183c
Bekali <66372333+Baiterekov@users.noreply.github.com>
2023-01-09 09:23:16 +0600

    Add files via upload
    Given an integer number n, return the difference between the product of its digits and the sum of its digits.
     
    
    Example 1:
    
    Input: n = 234
    Output: 15 
    Explanation: 
    Product of digits = 2 * 3 * 4 = 24 
    Sum of digits = 2 + 3 + 4 = 9 
    Result = 24 - 9 = 15
    Example 2:
    
    Input: n = 4421
    Output: 21
    Explanation: 
    Product of digits = 4 * 4 * 2 * 1 = 32 
    Sum of digits = 4 + 4 + 2 + 1 = 11 
    Result = 32 - 11 = 21

---
+++ Subtract the Product and Sum of Digits of an Integer.py
@@ -0,0 +1,32 @@
+"""
+Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+ 
+
+Example 1:
+
+Input: n = 234
+Output: 15 
+Explanation: 
+Product of digits = 2 * 3 * 4 = 24 
+Sum of digits = 2 + 3 + 4 = 9 
+Result = 24 - 9 = 15
+Example 2:
+
+Input: n = 4421
+Output: 21
+Explanation: 
+Product of digits = 4 * 4 * 2 * 1 = 32 
+Sum of digits = 4 + 4 + 2 + 1 = 11 
+Result = 32 - 11 = 21
+"""
+
+n = 4421
+
+str_n = str(n)
+sum_digits = 0
+multiply_digits = 1
+for i in str_n:
+	sum_digits = sum_digits + int(i)
+	multiply_digits = multiply_digits*int(i)
+
+print(multiply_digits - sum_digits)
