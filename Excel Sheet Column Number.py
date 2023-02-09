5f7ae021b9c473aa7a75b5ecb1404fe29b2e90f5
Bekali <66372333+Baiterekov@users.noreply.github.com>
2023-02-09 11:16:35 +0600

    Add files via upload
    Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
    
    For example:
    
    A -> 1
    B -> 2
    C -> 3
    ...
    Z -> 26
    AA -> 27
    AB -> 28 
    ...
     
    
    Example 1:
    
    Input: columnTitle = "A"
    Output: 1
    Example 2:
    
    Input: columnTitle = "AB"
    Output: 28
    Example 3:
    
    Input: columnTitle = "ZY"
    Output: 701

---
+++ Excel Sheet Column Number.py
@@ -0,0 +1,36 @@
+"""
+Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+
+For example:
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ 
+
+Example 1:
+
+Input: columnTitle = "A"
+Output: 1
+Example 2:
+
+Input: columnTitle = "AB"
+Output: 28
+Example 3:
+
+Input: columnTitle = "ZY"
+Output: 701
+"""
+
+columnTitle = "ZY"
+number = 0
+
+for i in columnTitle:
+	number = 26*number+ord(i)-64
+print(number)
+
